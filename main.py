b17c62597bb20c8e0a058cc007b751f886e2c47c
WMaxZimmerman <william.m.zimmerman+github@gmail.com>
2020-05-14 10:29:10 -0500

    changed to cli execution pattern

--- main.py
+++ main.py
@@ -1,18 +1,10 @@
-from tkinter import Tk, Canvas, NW, Button, Toplevel, Text, WORD, INSERT
-from cv2 import cvtColor, imread, COLOR_BGR2RGB
-import numpy as np
-import PIL.Image
-import PIL.ImageTk
-from csv import *
-from os.path import join, abspath
-import sys
-import urllib.request
+from tkinter import Tk
 from pathlib import Path
 import get_agent_choice as gac
 import helpers.resource_helper as rh
 
 
-def startUp():
+def run():
     imgDirPath = rh.get_resource_path('Images')
     Path(imgDirPath).mkdir(exist_ok=True)
 
@@ -22,9 +14,11 @@ def startUp():
     locImgDirPath = rh.get_resource_path('LocationImages')
     Path(locImgDirPath).mkdir(exist_ok=True)
 
+    window = Tk()
+    window.title('Map Choice')
 
-startUp()
-window = Tk()
-window.title('Map Choice')
+    start = gac.getAgentChoice(window)
 
-start = gac.getAgentChoice(window)
+
+if __name__ == "__main__":
+    run()
